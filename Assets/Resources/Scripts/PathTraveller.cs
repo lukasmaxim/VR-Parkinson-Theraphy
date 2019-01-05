87a54337f7e907e967fa1c3f02ec1e478bdfa0fc
David Bauer <david.bauer009@gmail.com>
2019-01-05 17:00:08 +0100

    Pathtraveller added

---
+++ Assets/Resources/Scripts/PathTraveller.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathTraveller : MonoBehaviour
+{
+
+    public Transform[] path;
+    public float speed;
+
+    int currentNode;
+    float timer;
+
+    bool shouldFollow = true;
+
+    public void FollowPath()
+    {
+        currentNode = 0;
+        timer = 0;
+        shouldFollow = true;
+
+    }
+
+    void checkNextNode()
+    {
+        if (currentNode == path.Length - 1)
+        {
+            shouldFollow = false;
+        }
+        else
+        {
+            currentNode++;
+            timer = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!shouldFollow)
+        {
+            return;
+        }
+
+        timer += Time.deltaTime * speed;
+        if (this.transform.position != path[currentNode].position)
+        {
+            this.transform.position = Vector3.Slerp(this.transform.position, path[currentNode].position, timer);
+        }
+        else
+        {
+            checkNextNode();
+        }
+    }
+}
